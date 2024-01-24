796a63b2e4472e6c0c28132bad616ca31ea4bd3a
Juan Pablo Sala <jpsala@gmail.com>
2024-01-23 21:42:19 -0300

    refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,203 +1,227 @@
-import { useEffect, useMemo, useState } from 'react'
-import dayjs from 'dayjs'
-import './App.css'
+import { useEffect, useState } from 'react';
+import dayjs from 'dayjs';
+import './App.css';
 
-function App() {
-  const [loading, setLoading] = useState(0)
-  const [type, setType] = useState<'USD' | 'ARS'>('USD')
-  const [usd1, setUsd1] = useState('')
-  const [ars1, setArs1] = useState('')
-  const [usd2, setUsd2] = useState('')
-  const [ars2, setArs2] = useState('')
-  const [usd3, setUsd3] = useState('')
-  const [ars3, setArs3] = useState('')
-  const [state, setState] = useState({ compra: 0, venta: 0, fecha: '', valor: 0 })
-  const [state2, setState2] = useState({ compra: 0, venta: 0, fecha: '', valor: 0 })
-  const [state3, setState3] = useState({ compra: 0, venta: 0, fecha: '', valor: 0 })
-  const format = (val: number) => new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  }).format(val)
-  const total1 = useMemo<string>(()=>{
-    if(type === 'USD'){
-      return String(Number(usd1.toString().slice(-1) === '.' ? usd1 + '0' : usd1) * state.valor)
-    } else {
-      return String(Number(ars1.toString().slice(-1) === '.' ? ars1 + '0' : ars1) / state.valor)
-    }
-  }, [type, usd1, ars1, state.valor])
+type Rate = {
+  value_buy: number;
+  value_sell: number;
+  value_avg: number;
+};
 
-  const total2 = useMemo<string>(()=>{
-    if(type === 'USD'){
-      return String(Number(usd2.toString().slice(-1) === '.' ? usd2 + '0' : usd2) * state2.valor)
-    } else {
-      return String(Number(ars2.toString().slice(-1) === '.' ? ars2 + '0' : ars2) / state2.valor)
-    }
-  }, [type, usd2, ars2, state2.valor])
+type BlueApiResponse = {
+  oficial: Rate;
+  blue: Rate;
+  oficial_euro: Rate;
+  blue_euro: Rate;
+  last_update: string;
+};
 
-  const total3 = useMemo<string>(()=>{
-    if(type === 'USD'){
-      return String(Number(usd3.toString().slice(-1) === '.' ? usd3 + '0' : usd3) * state3.valor)
-    } else {
-      return String(Number(ars3.toString().slice(-1) === '.' ? ars3 + '0' : ars3) / state3.valor)
-    }
-  }, [type, usd3, ars3, state3.valor])
+type DolarApiResponse = {
+  compra: number;
+  venta: number;
+  fechaActualizacion: string;
+};
 
-  useEffect(() => {
+type AmbitoApiResponse = {
+  compra: string;
+  venta: string;
+  fecha: string;
+  variacion: string;
+  classVariacion: string;
+  valor_cierre_ant: string;
+};
 
-    setLoading(val => val+ 1)
-    fetch('https://api.bluelytics.com.ar/v2/latest') .then(res => res.json())
-    .then(data => {
-      const [compra, venta] = [Number(data.blue.value_buy), Number(data.blue.value_sell)]
-      const valor = (compra+venta)/2
-      setState( { fecha: dayjs(data.last_update).format('DD/MMM/YYYY'), compra,  venta, valor })
-    }).catch((e) => {
-      console.log('error https://api.bluelytics.com.ar/v2/latest', e.toString())
-    }).finally(() => {
-      setLoading(val => val - 1)
-    })
+type DolarsiCasa = {
+  compra: string;
+  venta: string;
+  nombre: string;
+  // include other fields if necessary
+};
+
+type DolarsiApiResponse = Array<{ casa: DolarsiCasa }>;
+
+type ApiResponseType = BlueApiResponse | DolarApiResponse | AmbitoApiResponse | DolarsiApiResponse
+
+type ExchangeRate = {
+  compra: number;
+  venta: number;
+  fecha: string;
+  valor: number;
+};
+
+type EndpointConfig = {
+  label: string;
+  url: string;
+  parseData: (data: ApiResponseType) => ExchangeRate;
+};
+
+const App: React.FC = () => {
+  const [loading, setLoading] = useState<number>(0);
+  const [type, setType] = useState<'USD' | 'ARS'>('USD');
+  const [amount, setAmount] = useState<string>('');
+  const [exchangeRates, setExchangeRates] = useState<ExchangeRate[]>([]);
+  const endpoints: EndpointConfig[] = [
+    {
+      label: 'Ambito Financiero',
+      url: 'https://mercados.ambito.com//dolar/informal/variacion',
+      parseData: (data: ApiResponseType): ExchangeRate => {
+        if ('compra' in data && 'venta' in data && typeof data.compra === 'string' && typeof data.venta === 'string' && 'fecha' in data) {
+          return {
+            compra: parseFloat(data.compra.replace(',', '.')),
+            venta: parseFloat(data.venta.replace(',', '.')),
+            fecha: data.fecha,
+            valor: (parseFloat(data.compra.replace(',', '.')) + parseFloat(data.venta.replace(',', '.'))) / 2
+          };
+        }
+        throw new Error("Data does not match AmbitoApiResponse structure");
+      }
+    },
+    {
+      label: 'BlueLytics',
+      url: 'https://api.bluelytics.com.ar/v2/latest',
+      parseData: (data: ApiResponseType): ExchangeRate => {
+        if ('blue' ! in data) {
+          return {
+            compra: data.blue.value_buy,
+            venta: data.blue.value_sell,
+            fecha: dayjs(data.last_update).format('DD/MMM/YYYY'),
+            valor: data.blue.value_avg
+          };
+        }
+        throw new Error("Data does not match BlueApiResponse structure");
+      }
+    },
+    {
+      label: 'DolarAPI',
+      url: 'https://dolarapi.com/v1/dolares/blue',
+      // Here parseData should explicitly expect a DolarApiResponse
+      parseData: (data: ApiResponseType): ExchangeRate => {
+        if ('compra' in data && 'venta' in data && 'fechaActualizacion' in data) {
+          return {
+            compra: data.compra,
+            venta: data.venta,
+            fecha: dayjs(data.fechaActualizacion).format('DD/MMM/YYYY'),
+            valor: (data.compra + data.venta) / 2
+          };
+        }
+        throw new Error("Data does not match DolarApiResponse structure");
+      }
+    },
+    {
+      label: 'DolarSi',
+      url: 'https://www.dolarsi.com/api/api.php?type=valoresprincipales',
+      parseData: (data: ApiResponseType): ExchangeRate => {
+        const dolarBlueData = Array.isArray(data) ? data.find(item => item.casa.nombre === "Dolar Blue")?.casa : null
+  
+        if (dolarBlueData) {
+          return {
+            compra: parseFloat(dolarBlueData.compra.replace('.', '').replace(',', '.')),
+            venta: parseFloat(dolarBlueData.venta.replace('.', '').replace(',', '.')),
+            fecha: '', // The API response doesn't include a date field for Dolar Blue
+            valor: (parseFloat(dolarBlueData.compra.replace('.', '').replace(',', '.')) + parseFloat(dolarBlueData.venta.replace(',', '.'))) / 2
+          };
+        }
+  
+        throw new Error("Dolar Blue data not found in DolarsiApiResponse structure");
+      }
+    },
     
-    setLoading(val => val+ 1)
-    fetch('https://dolarapi.com/v1/dolares/blue') .then(res => res.json())
-    .then(data => {
-      const [compra, venta] = [Number(data.compra), Number(data.venta)]
-      const valor = (compra+venta)/2
-      setState2( { fecha: dayjs(data.fechaActualizacion).format('DD/MMM/YYYY'), compra,  venta, valor })
-    }).catch((e) => {
-      console.log('error https://dolarapi.com/v1/dolares/blue', e.toString())
-    }).finally(() => {
-      setLoading(val => val - 1)
-    })
+  ];
+  
+  useEffect(() => {
+    let isMounted = true;
+    setLoading(endpoints.length);
+    const newExchangeRates: ExchangeRate[] = [];
 
-    setLoading(val => val+ 1)
-    fetch('https://api.bluelytics.com.ar/v2/latest') .then(res => res.json())
-    .then(data => {
-      console.log('data', data)
-      const [compra, venta] = [Number(data.blue.value_buy), Number(data.blue.value_sell)]
-      setState3( { fecha: dayjs(data.last_update).format('DD/MMM/YYYY'),
-        compra,  
-        venta,
-        valor: data.blue.value_avg 
-      })
-    }).catch((e) => {
-       console.log(e.toString());
-      console.log('error https://api.bluelytics.com.ar/v2/latest', e.toString())
-    }).finally(() => {
-      setLoading(val => val - 1)
-    })
+    endpoints.forEach((endpoint, index) => {
+      fetch(endpoint.url)
+        .then(res => res.json())
+        .then((data: ApiResponseType) => {
+          if (isMounted) {
+            newExchangeRates[index] = endpoint.parseData(data);
+            if (index === endpoints.length - 1) {
+              setExchangeRates(newExchangeRates);
+            }
+          }
+        })
+        .catch(e => {
+          console.error(`Error fetching data from ${endpoint.url}:`, e);
+        })
+        .finally(() => {
+          if (isMounted) {
+            setLoading((prev: number) => prev - 1);
+          }
+        });
+    });
+
+    return () => {
+      isMounted = false;
+    };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  const formatCurrency = (val: number) => {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(val);
+  };
+
+  const calculateTotal = (valor: number) => {
+    console.log("Calculating total with valor:", valor, "and amount:", amount);
+    const numericAmount = parseFloat(amount) || 0;
+    const total = type === 'USD' ? (numericAmount * valor) : (numericAmount / valor);
+    console.log("Calculated total:", total);
+    return total;
+  };
 
   return (
     <>
       <div className="mt-10 border-gray-300 border p-8 rounded-md flex flex-col gap-2 mx-auto" style={{width: '480px'}}>
         {loading ? (
           <h1>Loading....</h1>
-          ) : (
-            <>
-              <h1 className='font-bold text-2xl'>Dolares JP</h1>
-              <hr className='mt-2 mb-2'/>
-              <div>
-                <label htmlFor="USD"  style={{minWidth: '115px', display: 'inline-block'}}>Dolares:</label>
-                <input autoFocus={true} autoComplete='off' id="USD" type="text"  className='ml-3 border border-gray-300 rounded-md p-1'
-                  value={usd1}
-                  onChange={e => {
-                    setType('USD')
-                    setArs1('')
-                    setArs2('')
-                    setArs3('')
-                    setUsd1(e.target.value)
-                    setUsd2(e.target.value)
-                    setUsd3(e.target.value)
+        ) : (
+          <>
+            <h1 className='font-bold text-2xl'>Dolares JP</h1>
+            <hr className='mt-2 mb-2'/>
+            <div>
+              <label htmlFor="USD" style={{minWidth: '115px', display: 'inline-block'}}>Amount:</label>
+              <input autoFocus={true} autoComplete='off' id="USD" type="text" className='ml-3 border border-gray-300 rounded-md p-1'
+                value={amount}
+                onChange={e => {
+                  setType('USD');
+                  setAmount(e.target.value);
                 }}/>
-              </div>
-              <div>
-                <label htmlFor='ARS' style={{minWidth: '115px', display: 'inline-block'}}>Pesos:</label>
-                <input id='ARS' type="text" className='ml-3 border border-gray-300 rounded-md p-1'
-                  autoComplete='off' value={ars1}
-                  onChange={e => {
-                    setType('ARS')
-                    setUsd1('')
-                    setUsd2('')
-                    setUsd3('')
-                    setArs1(e.target.value)
-                    setArs2(e.target.value)
-                    setArs3(e.target.value)
-                }}/>
-              </div>
-              <hr className='mt-2 mb-2'/>
-              <span className='text-md font-bold mb-3'>
-                api.bluelytics.com.ar al {`${state.fecha}`}
-              </span>
-              <div className='flex flex-col'>
-                <div className='flex w-full'>
-                  <span className='text-sm font-bold w-[120px] text-right'>Compra</span>
-                  <span className='text-sm font-bold w-[120px] text-right'>Venta</span>
-                  <span className='text-sm font-bold w-[120px] text-right'>Promedio</span>
-                  <span className='text-sm font-bold w-[120px] text-right'>Total</span>
-                </div>
-                <div className='flex w-full'>
-                  <span className='text-sm w-[120px] text-right'>{format(state.compra)}</span>
-                  <span className='text-sm w-[120px] text-right'>{format(state.venta)}</span>
-                  <span className='text-sm w-[120px] text-right'>{format(state.valor)}</span>
-                  <span className='text-sm w-[120px] text-right'>{format(Number(total1))}</span>
-                </div>
-              </div>
-              <div className='ml-auto'>
-                  <span style={{minWidth: '132px', display: 'inline-block'}}>Total:</span>
-                  <span className='text-lg font-bold'>{format(Number(total1))}</span>
-                </div>
+            </div>
+            <hr className='mt-2 mb-2'/>
 
-              <hr className='mt-2 mb-2'/>
-                
-              <span className='text-md font-bold mb-3'>
-                Dolarapi.com al {state2.fecha}
-              </span>
-              <div className='flex flex-col'>
-                <div className='flex w-full'>
-                  <span className='text-sm font-bold w-[120px] text-right'>Compra</span>
-                  <span className='text-sm font-bold w-[120px] text-right'>Venta</span>
-                  <span className='text-sm font-bold w-[120px] text-right'>Promedio</span>
-                  <span className='text-sm font-bold w-[120px] text-right'>Total</span>
+            {exchangeRates.map((rate, index) => (
+              <div key={index}>
+                <span className='text-md font-bold mb-3'>
+                  {endpoints[index].label} al {rate.fecha}
+                </span>
+                <div className='flex flex-col'>
+                  <div className='flex w-full'>
+                    <span className='text-sm font-bold w-[120px] text-right'>Compra</span>
+                    <span className='text-sm font-bold w-[120px] text-right'>Venta</span>
+                    <span className='text-sm font-bold w-[120px] text-right'>Promedio</span>
+                    <span className='text-sm font-bold w-[120px] text-right'>Total</span>
+                  </div>
+                  <div className='flex w-full'>
+                    <span className='text-sm w-[120px] text-right'>{formatCurrency(rate.compra)}</span>
+                    <span className='text-sm w-[120px] text-right'>{formatCurrency(rate.venta)}</span>
+                    <span className='text-sm w-[120px] text-right'>{formatCurrency(rate.valor)}</span>
+                    <span className='text-sm w-[120px] text-right'>{formatCurrency(calculateTotal(rate.valor))}</span>
+                  </div>
                 </div>
-                <div className='flex w-full'>
-                  <span className='text-sm w-[120px] text-right'>{format(state2.compra)}</span>
-                  <span className='text-sm w-[120px] text-right'>{format(state2.venta)}</span>
-                  <span className='text-sm w-[120px] text-right'>{format(state2.valor)}</span>
-                  <span className='text-sm w-[120px] text-right'>{format(Number(total2))}</span>
-                </div>
+                <hr className='mt-2 mb-2'/>
               </div>
-              <div className='ml-auto'>
-                <span style={{minWidth: '132px', display: 'inline-block'}}>Total:</span>
-                <span className='text-lg font-bold'>{format(Number(total2))}</span>
-              </div>
-
-              <hr className='mt-2 mb-2'/>
-                
-              <span className='text-md font-bold mb-3'>
-              api.bluelytics.com.ar {state2.fecha}
-              </span>
-              <div className='flex flex-col'>
-                <div className='flex w-full'>
-                  <span className='text-sm font-bold w-[120px] text-right'>Compra</span>
-                  <span className='text-sm font-bold w-[120px] text-right'>Venta</span>
-                  <span className='text-sm font-bold w-[120px] text-right'>Promedio</span>
-                  <span className='text-sm font-bold w-[120px] text-right'>Total</span>
-                </div>
-                <div className='flex w-full'>
-                  <span className='text-sm w-[120px] text-right'>{format(state3.compra)}</span>
-                  <span className='text-sm w-[120px] text-right'>{format(state3.venta)}</span>
-                  <span className='text-sm w-[120px] text-right'>{format(state3.valor)}</span>
-                  <span className='text-sm w-[120px] text-right'>{format(Number(total3))}</span>
-                </div>
-              </div>
-              <div className='ml-auto'>
-                <span style={{minWidth: '132px', display: 'inline-block'}}>Total:</span>
-                <span className='text-lg font-bold'>{format(Number(total3))}</span>
-              </div>
-            </>
-          )}
+            ))}
+          </>
+        )}
       </div>
     </>
-    )
+  );
 }
 
-export default App
+export default App;
